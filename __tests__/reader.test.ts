c9326799d760d9667b3b48971460a03f79b38d42
Spenser Black <spenserblack01@gmail.com>
2022-05-09 14:01:41 -0400

    Add tests for `readBytes`

--- __tests__/reader.test.ts
+++ __tests__/reader.test.ts
@@ -68,6 +68,30 @@ describe('Reader', () => {
     });
   });
 
+  describe('readByte', () => {
+    test('reads two bytes', () => {
+      const bytes = new Uint8Array([0xAB, 0x12, 0x89]);
+      const reader = new Reader(bytes);
+
+      expect(reader.readBytes(2)).toEqual([0xAB, 0x12]);
+    });
+
+    test('does not read when there is a partial unread byte', () => {
+      const bytes = new Uint8Array([0xAB, 0xCD]);
+      const reader = new Reader(bytes);
+      reader.readBit();
+
+      expect(() => reader.readBytes(1)).toThrow();
+    });
+
+    test('throws when not enough bytes to read', () => {
+      const bytes = new Uint8Array([0xAB, 0xCD]);
+      const reader = new Reader(bytes);
+
+      expect(() => reader.readBytes(3)).toThrow();
+    });
+  });
+
   describe('readAll', () => {
     describe('Uint8', () => {
       const bytes = new Uint8Array([0x12, 0x34, 0x56]);
